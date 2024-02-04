329aac6c945970fb9f2dcecf58a897ede9d86e72
romain-mezghenna <romain.mezghenna@etu.umontpellier.fr>
2024-02-04 20:59:33 +0100

    bcrypt

--- src/utils/comparePassword.js
+++ src/utils/comparePassword.js
@@ -1,2 +1,3 @@
 const bcrypt = require('bcrypt');
-module.exports = (password, hashedPassword) => { return bcrypt.compare(password, hashedPassword); }
+const salt = "a1z2e3r4t5y6u7i8o9p0";
+module.exports = (password, hashedPassword) => { return bcrypt.compare(password + salt, hashedPassword); }
--- src/utils/hashPassword.js
+++ src/utils/hashPassword.js
@@ -1,12 +1,15 @@
 const bcrypt = require('bcrypt');
 
 module.exports = async (password) => {
-    // Ajouter un chiffre aléatoire au mot de passe pour renforcer la sécurité
-    password += Math.floor(Math.random() * 100);
+    // Ajouter une constante au mot de passe pour le rendre plus sécurisé
+    const salt = "a1z2e3r4t5y6u7i8o9p0";
+    password = password + salt;
     const saltRounds = 10; // Vous pouvez ajuster le nombre de rounds
     try {
         const hashedPassword = await bcrypt.hash(password, saltRounds);
+        console.log(hashedPassword)
         return hashedPassword;
+        
     } catch (error) {
         throw error;
     }
